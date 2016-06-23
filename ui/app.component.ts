8c053eeda0ab219860615796b5b231bfd800c9cc
Mwaa Joseph <mwaa@ralphowino.com>
2016-06-23 14:36:21 +0300

    minor updates

--- ui/app.component.ts
+++ ui/app.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
 
 import { pluginStructure } from "/admin/hooks.ts";
+//TODO:: uncommented once hook_wrapper issue is addressed
+// import { pluginStructure } from "/admin/hook_wrapper.ts";
 
 @Component({
     selector: 'bpm-app',
--- ui/hooks.ts
+++ ui/hooks.ts
@@ -42,3 +42,5 @@ export const pluginMenus = [
         type: 'main'
     }
 ];
+
+export  function initWeb(){}
